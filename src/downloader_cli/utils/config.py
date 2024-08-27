e267505905bb3af72c0bb47a71dddbcbb556e660
tadeasf <taddy.fort@gmail.com>
2024-08-28 01:52:15 +0200

    📝 (utils): add Apache License 2.0 header to __init__.py and config.py files The Apache License 2.0 header has been added to the __init__.py and config.py files to ensure proper licensing compliance and provide clarity on the permitted use of the code. This addition helps in clearly defining the terms under which the software can be used, distributed, and modified.

--- src/downloader_cli/utils/config.py
+++ src/downloader_cli/utils/config.py
@@ -1,3 +1,17 @@
+# Copyright 2024 tadeasfort
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
 import yaml
 from typing import Dict, Any
@@ -14,7 +28,11 @@ def load_config() -> Dict[str, Any]:
         return create_default_config()
 
     with open(CONFIG_FILE, "r") as f:
-        return yaml.safe_load(f)
+        config = yaml.safe_load(f)
+
+    # Update version from pyproject.toml
+    config["version"] = get_version_from_pyproject()
+    return config
 
 
 def create_default_config() -> Dict[str, Any]:
